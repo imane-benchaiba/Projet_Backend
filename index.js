ed1f7aadaf0ba7631121546f026f79f61b7d006b
imane-benchaiba <imyben.bi@gmail.com>
2021-06-18 00:18:57 +0200

    Modifier messages d'erreurs

--- index.js
+++ index.js
@@ -6,10 +6,18 @@ require('dotenv').config({path: './config/.env'});
 require('./config/db');
 const {checkUser, requireAuth} = require('./middleware/auth.middleware');
 const app = express();
+const cors = require('cors');
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
 // jwt
 app.get('*', checkUser); // dans chaque requete on doit vérifier si c'est bien le user concerné
--- utils/errors.utils.js
+++ utils/errors.utils.js
@@ -11,10 +11,10 @@ module.exports.signUpErrors = (err) => {
         errors.password = 'Le mot de passe doit contenir 8 catactères minimum';
 
     if (err.code === 11000 && Object.keys(err.keyValue)[0].includes('pseudo'))
-        errors.pseudo = 'Ce pseudo est déjà enregistré';
+        errors.pseudo = 'Ce pseudo existe déjà';
 
     if (err.code === 11000 && Object.keys(err.keyValue)[0].includes('email'))
-        errors.email = 'Cet email est déjà enregistré';
+        errors.email = 'Cet email existe déjà';
     return errors;
 }
 
@@ -23,7 +23,7 @@ module.exports.signInErrors = (err) => {
 
     if(err.message.includes('email')) errors.email = 'Email inconnu';
 
-    if(err.message.includes('password')) errors.password = 'Le mot de passe est incorrect';
+    if(err.message.includes('password')) errors.password = 'Mot de passe incorrect';
 
     return errors;
 }
